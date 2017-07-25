2cd350b5e5822bcd8d0f3077b2430afd2da39e1d
Howard <chenhao_wang@163.com>
2017-07-25 11:00:24 +0800

    temp stor fin
    finalizing editor temporary storage
    added simple anti-ddos validation

--- routers/editor.js
+++ routers/editor.js
@@ -77,6 +77,12 @@ var isLoggedIn = function (req, res, next) {
     res.redirect('/auth/login')
 }
 
+function frequentMessage(res) {
+    res.writeHead(403)
+    res.write("Too frequent operations")
+    res.end()
+}
+
 var message = {
     userName: String,
     authorName: String,
@@ -84,128 +90,224 @@ var message = {
     hasTemp: Boolean
 }
 
+var userOpTime = new Map()
+var userWriteTime = new Map()
+var userAutoTime = new Map()
+
 
 module.exports = function (app) {
     //editor page
     //new post
     app.get('/editor/new', isLoggedIn, (req, res) => {
+        var nowTime = new Date()
         var user = req.user
-        message.userName = req.userName
-        tempArticle.findOne({ 'header.authorName': user.local.userName, 'header.derivedFrom': null }, (err, tempArticles) => {
-            if (tempArticles) {
-                message.hasTemp = true
-                res.render('../views/editor.pug', { user: user, message: message, article: tempArticles })
-
+        var authorized = false
+        message.userName = req.user.local.userName
+        if (userOpTime.get(message.userName)) {
+            var lastOpTime = userOpTime.get(message.userName)
+            if (nowTime - lastOpTime >= 15000) {
+                authorized = true
+                userOpTime.set(message.userName, nowTime)
             } else {
-                message.hasTemp = false
-                res.render('../views/editor.pug', { user: user, message: message, article: null })
+                frequentMessage(res)
             }
-        })
+        } else {
+            userOpTime.set(message.userName, nowTime)
+            authorized = true
+        }
+        if (authorized) {
+            tempArticle.findOne({ 'header.authorName': user.local.userName, 'header.derivedFrom': null }, (err, tempArticles) => {
+                if (tempArticles) {
+                    message.hasTemp = true
+                    res.render('../views/editor.pug', { user: user, message: message, article: tempArticles })
+                } else {
+                    message.hasTemp = false
+                    res.render('../views/editor.pug', { user: user, message: message, article: null })
+                }
+            })
+        } else {
+            frequentMessage(res)
+
+        }
+
     })
     //save new post
     app.post('/editor/new', isLoggedIn, (req, res) => {
+        var nowTime = new Date()
         var user = req.user
         var title = req.body.title
         var content = req.body.content
-        storeArticle(undefined, user, title, content, article => {
-            var id = article.header.articleId
-            res.redirect('/content/' + id)
-            //delete tempdata after submit
-            tempArticle.deleteOne({ 'header.authorName': user.local.userName, 'header.derivedFrom': null }, (err, res) => {
-                if (err) {
-                    throw err
-                }
-            })
-        })
+        var authorized = false
+        if (userWriteTime.get(user.local.userName)) {
+            var lastWriteTime = userWriteTime.get(user.local.userName)
+            if (nowTime - lastWriteTime >= 15000) {
+                authorized = true
+                userOpTime.set(user.local.userName, nowTime)
+            } else {
+                frequentMessage(res)
 
+            }
+
+        } else {
+            userOpTime.set(user.local.userName, nowTime)
+            authorized = true
+        }
+        if (authorized) {
+            storeArticle(undefined, user, title, content, article => {
+                var id = article.header.articleId
+                res.redirect('/content/' + id)
+                //delete tempdata after submit
+                tempArticle.deleteOne({ 'header.authorName': user.local.userName, 'header.derivedFrom': null }, (err, res) => {
+                    if (err) {
+                        throw err
+                    }
+                })
+            })
+        } else {
+            frequentMessage(res)
+
+        }
     })
     //auto save
     app.post('/editor/autoSave', isLoggedIn, (req, res) => {
+        var nowTime = new Date()
+        var authorized = false
         var user = req.user
         var title = req.body.title
         var content = req.body.content
         var derivedFrom = req.query.derive
+        var autoKey = user.local.userName+"/"+derivedFrom
         if (derivedFrom === 'new') {
             derivedFrom = undefined
         }
-        if (derivedFrom) {
-            tempArticle.findOne({ 'header.derivedFrom': derivedFrom }, (err, tempArticles) => {
-                if (tempArticles) {
-                    storeTempArticle(tempArticles, derivedFrom, user, title, content, (tempArti) => {
-                        res.writeHead(200)
-                        res.end()
-                    })
-                } else {
-                    storeTempArticle(undefined, derivedFrom, user, title, content, (tempArti) => {
-                        res.writeHead(200)
-                        res.end()
-                    })
-                }
-            })
+        if (userAutoTime.get(autoKey)) {
+            var lastAutoTime = userAutoTime.get(autoKey)
+            if (nowTime - lastAutoTime >= 50000) {
+                authorized = true
+                userAutoTime.set(autoKey, nowTime)
+            } else {
+                frequentMessage(res)
+            }
         } else {
-            tempArticle.findOne({ 'header.authorName': user.local.userName }, (err, tempArticles) => {
-                if (tempArticles) {
-                    storeTempArticle(tempArticles, undefined, user, title, content, (tempArti) => {
-                        res.writeHead(200)
-                        res.end()
-                    })
-                } else {
-                    storeTempArticle(undefined, undefined, user, title, content, (tempArti) => {
-                        res.writeHead(200)
-                        res.end()
-                    })
-                }
-            })
+            userAutoTime.set(autoKey, nowTime)
+            authorized = true
+        }
+        if (authorized) {
+            if (derivedFrom) {
+                tempArticle.findOne({ 'header.derivedFrom': derivedFrom }, (err, tempArticles) => {
+                    if (tempArticles) {
+                        storeTempArticle(tempArticles, derivedFrom, user, title, content, (tempArti) => {
+                            res.writeHead(200)
+                            res.end()
+                        })
+                    } else {
+                        storeTempArticle(undefined, derivedFrom, user, title, content, (tempArti) => {
+                            res.writeHead(200)
+                            res.end()
+                        })
+                    }
+                })
+            } else {
+                tempArticle.findOne({ 'header.authorName': user.local.userName }, (err, tempArticles) => {
+                    if (tempArticles) {
+                        storeTempArticle(tempArticles, undefined, user, title, content, (tempArti) => {
+                            res.writeHead(200)
+                            res.end()
+                        })
+                    } else {
+                        storeTempArticle(undefined, undefined, user, title, content, (tempArti) => {
+                            res.writeHead(200)
+                            res.end()
+                        })
+                    }
+                })
 
+            }
+        } else {
+            frequentMessage(res)
         }
     })
     //edit post
     app.get('/editor/:articleId', isLoggedIn, (req, res) => {
         var articleIdParam = req.params.articleId
         var user = req.user
-        Article.findOne({ 'header.articleId': articleIdParam }, (err, article) => {
+        tempArticle.findOne({ 'header.derivedFrom': articleIdParam, 'header.authorName': user.local.userName }, (err, tempArticles) => {
             if (err) {
                 throw err
             }
-            if (article) {
-                if (user.local.userName === article.header.authorName) {
-                    message.authorName = article.header.authorName
-                    message.postId = article.header.articleId
-                    res.render('../views/editor.pug', { user: user, message: message, article: article })
-                }
+            if (tempArticles) {
+                message.authorName = tempArticles.header.authorName
+                message.postId = tempArticles.header.derivedFrom
+                res.render('../views/editor.pug', { user: user, message: message, article: tempArticles })
             } else {
-                res.render('../views/404.pug')
+                Article.findOne({ 'header.articleId': articleIdParam }, (err, article) => {
+                    if (err) {
+                        throw err
+                    }
+                    if (article) {
+                        if (user.local.userName === article.header.authorName) {
+                            message.authorName = article.header.authorName
+                            message.postId = article.header.articleId
+                            res.render('../views/editor.pug', { user: user, message: message, article: article })
+                        } else {
+                            res.writeHead(403)
+                            res.write("Unauthorized access")
+                            res.end()
+                        }
+                    } else {
+                        res.render('../views/404.pug')
+                    }
+                })
+
             }
         })
     })
     //save edit
     app.post('/editor/:articleId', isLoggedIn, (req, res) => {
+        var nowTime = new Date()
         var articleIdParam = req.params.articleId
         var user = req.user
         var title = req.body.title
         var content = req.body.content
-        Article.findOne({ 'header.articleId': articleIdParam }, (err, article) => {
-            if (err) {
-                throw err
+        var authorized = false
+        if (userWriteTime.get(user.local.userName)) {
+            var lastWriteTime = userWriteTime.get(user.local.userName)
+            if (nowTime - lastWriteTime >= 150000) {
+                authorized = true
+                userWriteTime.set(user.local.userName, nowTime)
+            } else {
+                frequentMessage(res)
             }
-            if (article) {
-                if (user.local.userName === article.header.authorName) {
-                    storeArticle(article, user, title, content, article => {
-                        var id = article.header.articleId
-                        res.redirect('/content/' + id)
-                        tempArticle.deleteOne({ 'header.authorName': user.local.userName, 'header.derivedFrom': null }, (err, res) => {
-                            if (err) {
-                                throw err
-                            }
+        } else {
+            authorized = true
+            userWriteTime.set(user.local.userName, nowTime)
+        }
+        if (authorized) {
+            Article.findOne({ 'header.articleId': articleIdParam }, (err, article) => {
+                if (err) {
+                    throw err
+                }
+                if (article) {
+                    if (user.local.userName === article.header.authorName) {
+                        storeArticle(article, user, title, content, article => {
+                            var id = article.header.articleId
+                            res.redirect('/content/' + id)
+                            tempArticle.deleteOne({ 'header.authorName': user.local.userName, 'header.derivedFrom': id }, (err, res) => {
+                                if (err) {
+                                    throw err
+                                }
+                            })
                         })
-                    })
+                    } else {
+                        res.redirect('/')
+                    }
                 } else {
                     res.redirect('/')
                 }
-            } else {
-                res.redirect('/')
-            }
-        })
+            })
+        } else {
+            frequentMessage(res)
+        }
     })
 
     app.post('/editor/imageUpload', isLoggedIn, (req, res) => {
